f48cc6fa8017ff44ed02b51700d77659f3af3e93
hai_pham_minh <hai.phamminh86@gmail.com>
2019-11-05 10:31:13 +0700

    generate list & map

--- src/main/java/com/javassist/generator/PojoGenerator.java
+++ src/main/java/com/javassist/generator/PojoGenerator.java
@@ -3,9 +3,12 @@ package com.javassist.generator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.Serializable;
+import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicInteger;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -14,7 +17,13 @@ import javassist.CtMethod;
 import javassist.NotFoundException;
 
 public class PojoGenerator {
-    public static void main(String[] args) throws IOException, NotFoundException, CannotCompileException {
+    static final AtomicInteger counter = new AtomicInteger(1);
+    static final String MAIN_GENERATED_POJO_NAME = "com.javassist.generator.GeneratedPojo";
+    static final String NESTED_GENERATED_POJO_NAME = "com.javassist.generator.NestedGeneratedPojo";
+
+    public static void main(String[] args)
+            throws IOException, NotFoundException, CannotCompileException, IllegalAccessException,
+                   InstantiationException {
         String json = "{\n"
                 + "  \"key1\": 1,\n"
                 + "  \"key2\": true,\n"
@@ -25,12 +34,12 @@ public class PojoGenerator {
                 + "  \"key4\": \"value4\",\n"
                 + "  \"key5\": [\n"
                 + "    {\n"
-                + "      \"key511\": \"value511\",\n"
-                + "      \"key512\": \"value512\"\n"
+                + "      \"key51\": \"value511\",\n"
+                + "      \"key51\": \"value512\"\n"
                 + "    },\n"
                 + "    {\n"
-                + "      \"key521\": \"value521\",\n"
-                + "      \"key522\": \"value522\"\n"
+                + "      \"key51\": \"value513\",\n"
+                + "      \"key51\": \"value514\"\n"
                 + "    }\n"
                 + "  ]\n"
                 + "}";
@@ -38,33 +47,44 @@ public class PojoGenerator {
         Map<String, Object> map = mapper.readValue(json, Map.class);
         Map<String, Class<?>> properties = new HashMap<>();
         generateProperties(map, properties);
-        Class clazz = generate("com.javassist.generator.GeneratedPojo", properties);
+        Class clazz = generate(MAIN_GENERATED_POJO_NAME, properties);
+
+        Object obj = clazz.newInstance();
+
+        System.out.println("Clazz: " + clazz);
+        System.out.println("Object: " + obj);
+        System.out.println("Serializable? " + (obj instanceof Serializable));
+
+        for (final Method method : clazz.getDeclaredMethods()) {
+            System.out.println(method);
+        }
     }
 
-    static void generateProperties(Map<String, Object> map, Map<String, Class<?>> properties) {
+    static void generateProperties(Map<String, Object> map, Map<String, Class<?>> properties)
+            throws IOException, CannotCompileException, NotFoundException {
         for (Entry<String, Object> entry : map.entrySet()) {
             Object value = entry.getValue();
-            if (value.getClass()
-                     .isAssignableFrom(Map.class)) {
-                // TODO - nested object: List
-            } else if (value.getClass()
-                            .isAssignableFrom(Integer.class)) {
-                properties.put(entry.getKey(), Integer.class);
-            } else if (value.getClass()
-                            .isAssignableFrom(Long.class)) {
-                properties.put(entry.getKey(), Long.class);
-            } else if (value.getClass()
-                            .isAssignableFrom(Float.class)) {
-                properties.put(entry.getKey(), Float.class);
-            } else if (value.getClass()
-                            .isAssignableFrom(Double.class)) {
-                properties.put(entry.getKey(), Double.class);
-            } else if (value.getClass()
-                            .isAssignableFrom(Boolean.class)) {
-                properties.put(entry.getKey(), Boolean.class);
-            } else {
-                properties.put(entry.getKey(), String.class);
+            Class<?> cls = String.class;
+            if (isInstance(value, List.class)) {
+                cls = List.class;
             }
+            if (isInstance(value, Map.class)) {
+                Map<String, Class<?>> nestedProperties = new HashMap<>();
+                generateProperties((Map) value, nestedProperties);
+                String nestedClassName = NESTED_GENERATED_POJO_NAME + counter.getAndIncrement();
+                cls = generate(nestedClassName, nestedProperties);
+            } else if (isInstance(value, Integer.class)) {
+                cls = Integer.class;
+            } else if (isInstance(value, Long.class)) {
+                cls = Long.class;
+            } else if (isInstance(value, Float.class)) {
+                cls = Float.class;
+            } else if (isInstance(value, Double.class)) {
+                cls = Double.class;
+            } else if (isInstance(value, Boolean.class)) {
+                cls = Boolean.class;
+            }
+            properties.put(entry.getKey(), cls);
         }
     }
 
@@ -143,5 +163,9 @@ public class PojoGenerator {
     private static CtClass resolveCtClass(Class clazz) throws NotFoundException {
         ClassPool pool = ClassPool.getDefault();
         return pool.get(clazz.getName());
+    }
+
+    static boolean isInstance(Object value, Class<?> cls) {
+        return value != null && cls.isInstance(value);
     }
 }
